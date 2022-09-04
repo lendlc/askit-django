b134a93bf1d443f85882d86b6cb8ffeccf2477ba
lendlc <johnlendl.cuyugan20@gmail.com>
2022-09-05 02:56:36 +0800

    fix: db password

--- askit/settings.py
+++ askit/settings.py
@@ -99,7 +99,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'capstone_dev',
         'USER': 'root',
-        'PASSWORD': 'ubiqutiy1',
+        'PASSWORD': 'ubiquity1',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
