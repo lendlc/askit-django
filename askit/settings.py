98f322dfc80b954df4f2db08ccb6566bf1524b51
lendlc <johnlendl.cuyugan20@gmail.com>
2022-09-06 02:19:20 +0800

    feat: root static directory

--- askit/settings.py
+++ askit/settings.py
@@ -168,6 +168,8 @@ USE_TZ = False
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
